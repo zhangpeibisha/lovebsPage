d05056a9c41dceb4dceb01b0b236bbbfc4d9438e
1318699937@qq.com <1318699937@qq.com>
2019-05-09 16:51:39 +0800

    登录页面完善 2019/05/09 zsl

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,20 @@
 <template>
-  <div style="margin: auto">
-    <el-form :model="loginForm" status-icon
-             ref="loginForm" label-width="100px" class="demo-ruleForm"
+  <div style="margin: auto" class="login-template">
+    <el-form :model="loginForm"
+             status-icon
+             ref="loginForm"
+             label-width="100px"
+             id="loginForm"
+             class="demo-ruleForm"
              @submit.native.prevent>
 
       <el-form-item label="账号" prop="text">
-        <el-input type="text" v-model="loginForm.username"
+        <el-input class="qxs-ic_user" type="text" v-model="loginForm.username"
                   autocomplete="off" style="width: 200px" clearable></el-input>
       </el-form-item>
 
       <el-form-item label="密码" prop="pass">
-        <el-input type="password" v-model="loginForm.password"
+        <el-input class="qxs-ic_password" type="password" v-model="loginForm.password"
                   autocomplete="off" style="width: 200px" clearable show-password></el-input>
       </el-form-item>
 
@@ -33,7 +37,6 @@
 
 
 <script>
-
   export default {
     name: 'login',
     data() {
@@ -44,6 +47,9 @@
           image: ""
         }
       }
+    },
+    mounted () {
+      this.getIdentifyingCode();
     },
     methods: {
       handleLogin() {
@@ -67,3 +73,27 @@
     }
   }
 </script>
+
+<style>
+
+  .login-template{
+    width: 100%;
+    height: 100%;
+    background: url("../../assets/images/background.png") no-repeat center fixed;
+    background-size: cover;
+    position: fixed;
+  }
+
+#loginForm{
+  margin: 0 auto;
+  margin-top: 14%;
+  width: 367px;
+  padding-top: 23px;
+  height: 300px;
+  background-color: #ffffff;
+  opacity: 0.93;
+  border-radius: 10px;
+  /*background-color: #8b8b8b;*/
+}
+
+</style>
