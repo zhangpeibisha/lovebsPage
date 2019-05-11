8de8f59701ee94c255b974e2744e96dbfab2ab9b
zhangpei <zhangpe0312@qq.com>
2019-05-12 01:47:37 +0800

    提交代码

--- src/views/center/teacher.vue
+++ src/views/center/teacher.vue
@@ -13,6 +13,7 @@
       </el-main>
     </el-container>
 
+    <!--配置黑名单-->
     <el-dialog :title="'配置不计分学生'+'('+blacklist.canChoose+')'" :visible.sync="blacklist.dialogVisible">
       <el-form :model="blacklist">
         <el-form-item label="不计分学生" :label-width="blacklist.formLabelWidth">
@@ -49,6 +50,7 @@
         <el-button type="primary" @click="submitBlackList">确 定</el-button>
       </div>
     </el-dialog>
+    <!--发布问卷-->
 
 
     <el-tabs v-model="activeName" @tab-click="handleClick">
@@ -100,8 +102,6 @@
           @current-change="pendingChangePageSize">
         </el-pagination>
       </el-tab-pane>
-
-
       <el-tab-pane :label="'已经查看问卷'+'('+clickedQuestionIds.length+')'" name="second">
         <el-table :data="clickedQuestionList" style="width: 100%">
 
@@ -188,41 +188,53 @@
       <!--</el-tab-pane>-->
 
 
-      <!--<el-tab-pane label="编辑信息" name="fouth">-->
-      <!--<el-form ref="form" :model="form" label-width="80px">-->
-      <!--<el-form-item label="活动名称">-->
-      <!--<el-input v-model="form.name"></el-input>-->
-      <!--</el-form-item>-->
-      <!--<el-form-item label="活动区域">-->
-      <!--<el-select v-model="form.region" placeholder="请选择活动区域">-->
-      <!--<el-option label="区域一" value="shanghai"></el-option>-->
-      <!--<el-option label="区域二" value="beijing"></el-option>-->
-      <!--</el-select>-->
-      <!--</el-form-item>-->
-      <!--<el-form-item label="活动时间">-->
-      <!--<el-col :span="11">-->
-      <!--<el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>-->
-      <!--</el-col>-->
-      <!--<el-col class="line" :span="2">-</el-col>-->
-      <!--<el-col :span="11">-->
-      <!--<el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>-->
-      <!--</el-col>-->
-      <!--</el-form-item>-->
+      <el-tab-pane label="工作台">
+        <el-row>
+          <el-button type="success" plain>查看课程</el-button>
+          <el-button type="success" plain>问卷发布</el-button>
+        </el-row>
+      </el-tab-pane>
 
-      <!--<el-form-item>-->
-      <!--<el-button type="primary" @click="onSubmit">立即创建</el-button>-->
-      <!--<el-button>取消</el-button>-->
-      <!--</el-form-item>-->
-      <!--</el-form>-->
-      <!--</el-tab-pane>-->
+
+      <el-tab-pane label="编辑基本信息">
+        <el-form ref="form" :model="userInfo" label-width="80px" size="small">
+          <el-form-item label="电话">
+            <el-input v-model="userInfo.name" style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱">
+            <el-input v-model="userInfo.email" style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="updateUserInfo">提交修改</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="修改密码">
+        <el-form ref="form" :model="userInfo" label-width="80px" size="small">
+          <el-form-item label="旧密码">
+            <el-input v-model="userInfo.name" style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item label="新密码">
+            <el-input v-model="userInfo.email" style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item label="重复新密码">
+            <el-input v-model="userInfo.email" style="width: 20%"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="updatePassword">提交修改</el-button>
+            <el-button>取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+
     </el-tabs>
-
   </div>
 </template>
 
 <script>
-  import {findUserInfo, findPublishInfoByids, timestampToTime} from '@/api/center';
-  import {teacherClieckPublishQuestion, addBlackList} from '@/api/question'
+  import {findPublishInfoByids, findUserInfo, timestampToTime} from '@/api/center';
+  import {addBlackList, teacherClieckPublishQuestion} from '@/api/question'
 
   export default {
     data() {
@@ -351,9 +363,6 @@
           this.pendingQuestionListShow = this.pendingQuestionList.slice(startIndex, endIndex);
         }
       },
-      clickedChangePageSize(currPage) {
-
-      },
       handleClick() {
 
       },
@@ -381,7 +390,7 @@
           this.blacklist.canChoose = result.data.statisticsJson.canFilters;
           this.blacklist.teacher = [];
           this.blacklist.viewStudent = [];
-          var blackListStudentId =  result.data.statisticsJson.black || [];
+          var blackListStudentId = result.data.statisticsJson.black || [];
           result.data.statisticsJson.students.forEach(row => {
             row.index = this.blacklist.teacher.length;
             console.log("获取到的当前发布id:", cuurPublishId);
@@ -442,6 +451,12 @@
         }
       },
       handleDelete(index, row) {
+
+      },
+      updateUserInfo() {
+
+      },
+      updatePassword() {
 
       }
     }
