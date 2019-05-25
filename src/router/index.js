821f2798ce185f13fc610a4af0583d39173d99e8
kiss-yu <kiss4400>
2019-05-25 20:22:46 +0800

    权限

--- src/router/index.js
+++ src/router/index.js
@@ -7,19 +7,7 @@ Vue.use(Router)
 import Layout from '../views/layout/Layout'
 import Center from '../views/center/student'
 
-/**
- * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
- *                                if not set alwaysShow, only more than one route under the children
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    title: 'title'               the name show in submenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar,
-  }
- **/
-export const constantRouterMap = [
+const constantRouterMap = [
   {path: '/login', component: () => import('@/views/login/index'), hidden: false},
   {path: '/404', component: () => import('@/views/404'), hidden: true},
   {path: '/question/card/edit', component: () => import('@/views/question/card/index'), hidden: true},
@@ -169,10 +157,54 @@ export const constantRouterMap = [
     ]
   },
 ]
-
-export default new Router({
+const router =  new Router({
   mode: 'hash', //后端支持可开
   scrollBehavior: () => ({y: 0}),
   routes: constantRouterMap
+});
+
+const turnTo = (to, access, next) => {
+  if (canTurnTo(to.name, access, routes)) next() // 有权限，可访问
+  else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
+}
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  const token = getToken()
+  if (!token && to.name !== LOGIN_PAGE_NAME) {
+    // 未登录且要跳转的页面不是登录页
+    next({
+      name: LOGIN_PAGE_NAME // 跳转到登录页
+    })
+  } else if (!token && to.name === LOGIN_PAGE_NAME) {
+    // 未登陆且要跳转的页面是登录页
+    next() // 跳转
+  } else if (token && to.name === LOGIN_PAGE_NAME) {
+    // 已登录且要跳转的页面是登录页
+    next({
+      name: homeName // 跳转到homeName页
+    })
+  } else {
+    if (store.state.user.hasGetInfo) {
+      turnTo(to, store.state.user.access, next)
+    } else {
+      store.dispatch('getUserInfo').then(user => {
+        // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
+        turnTo(to, user.access, next)
+      }).catch(() => {
+        setToken('')
+        next({
+          name: 'login'
+        })
+      })
+    }
+  }
 })
+
+router.afterEach(to => {
+  setTitle(to, router.app)
+  iView.LoadingBar.finish()
+  window.scrollTo(0, 0)
+})
+export default router
 
